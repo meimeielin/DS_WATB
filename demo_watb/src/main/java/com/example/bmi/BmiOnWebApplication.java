af683a09007154a1603cffb06a77192fbe73f612
meimeielin <112306010@g.nccu.edu.tw>
2024-11-04 12:51:18 +0800

    20241104TA
    innitial

---
+++ demo_watb/src/main/java/com/example/bmi/BmiOnWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.bmi;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class BmiOnWebApplication {
+
+    public static void main(String[] args) {
+    SpringApplication.run(BmiOnWebApplication.class, args);
+}
+
+}
