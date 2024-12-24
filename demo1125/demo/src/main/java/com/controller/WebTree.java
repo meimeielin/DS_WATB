b3e892dc06414d0a529d5dc89f2f6768f6ddaf24
lu <starlit2843@gmail.com>
2024-12-24 17:14:59 +0800

    feat: tree and nodes added but not yet finished

--- demo1125/demo/src/main/java/com/controller/WebTree.java
+++ demo1125/demo/src/main/java/com/controller/WebTree.java
@@ -3,65 +3,39 @@ package com.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class WebTree{
-	public WebNode root;
+public class WebTree {
+    public WebNode root;
 
-	public WebTree(WebPage rootPage){
-		this.root = new WebNode(rootPage);
-	}
+    public WebTree(WebPage rootPage) {
+        this.root = new WebNode(rootPage);
+    }
 
-	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException{
-		setPostOrderScore(root, keywords);
-	}
+    // 递归爬取子网页
+    public void crawl(int depth) {
+        root.crawlChildren(depth);
+    }
 
-	private void setPostOrderScore(WebNode startNode, ArrayList<Keyword> keywords) throws IOException{
-		// YOUR TURN
-		// 3. compute the score of children nodes via post-order, then setNodeScore for
-		// startNode
-		
-		//獲取子節點列表
-		for (WebNode child : startNode.children) { 
-			setPostOrderScore(child, keywords); 
-		} 
-		// 設置當前節點的分數
-		startNode.setNodeScore(keywords);
-	    /* 將子節點的分數累加到父節點
-	    for (WebNode child : startNode.children) {
-	        startNode.nodeScore += child.nodeScore;
-	    }*/
-	 
-	}
-
-	public void eularPrintTree(){
-		eularPrintTree(root);
-	}
-
-	private void eularPrintTree(WebNode startNode){
-		int nodeDepth = startNode.getDepth();
-
-		if (nodeDepth > 1)
-			System.out.print("\n" + repeat("\t", nodeDepth - 1));
-
-		System.out.print("(");
-		System.out.print(startNode.webPage.name + "," + startNode.nodeScore);
-		
-		// YOUR TURN
-		// 4. print child via pre-order
-	    for (WebNode child : startNode.children) {
-	        eularPrintTree(child);  
-	    }
-		
-		System.out.print(")");
-
-		if (startNode.isTheLastChild())
-			System.out.print("\n" + repeat("\t", nodeDepth - 2));
-	}
-
-	private String repeat(String str, int repeat){
-		String retVal = "";
-		for (int i = 0; i < repeat; i++){
-			retVal += str;
+    // 计算整棵树的分数
+    public void setPostOrderScore(ArrayList<Keyword> keywords) {
+        try {
+			root.calculateNodeScore(keywords);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return retVal;
-	}
+    }
+
+    // 打印网页树
+    public void eularPrintTree() {
+        eularPrintTree(root, 0);
+    }
+
+    private void eularPrintTree(WebNode node, int depth) {
+        for (int i = 0; i < depth; i++) System.out.print("  ");
+        System.out.println(" (Score: " + node.nodeScore + ")");
+
+        for (WebNode child : node.children) {
+            eularPrintTree(child, depth + 1);
+        }
+    }
 }
