98b5f4cd4fcfdea868930066367dbf05c82d257f
chennaijia <naijia030@gmail.com>
2024-12-25 00:40:20 +0800

    still testing  googleRelatedSearch

---
+++ demo1125/demo/src/test/java/GoogleSearchTest.java
@@ -0,0 +1,31 @@
+package test.java;
+
+import com.example.project.config.WebDriverSetup;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // 使用 WebDriverSetup 初始化 driver
+        driver = WebDriverSetup.createDriver();
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com");
+        System.out.println("Page title is: " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // 測試結束後關閉瀏覽器
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
